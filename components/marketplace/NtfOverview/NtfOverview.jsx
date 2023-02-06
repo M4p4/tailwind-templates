6fa371527378a64b0dcd26c1aca21c5a33236dfe
M4p4 <homejr@protonmail.com>
2023-02-06 12:19:13 +0100

    wip

--- components/marketplace/NtfOverview/NtfOverview.jsx
+++ components/marketplace/NtfOverview/NtfOverview.jsx
@@ -1,8 +1,12 @@
+import { classNames } from '@/lib/helpers';
 import { HeartIcon, VideoCameraIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
-import React from 'react';
+import { useState } from 'react';
 
-const NtfOverview = () => {
+const NtfOverview = ({ nftData }) => {
+  const { likes } = nftData;
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="bg-black/20 py-20 lg:py-30">
       <div className="container mx-auto px-5">
@@ -28,8 +32,19 @@ const NtfOverview = () => {
                 </div>
               </div>
               <div className="absolute top-7 right-7">
-                <div className="bg-black/50 rounded-full px-4 py-2 text-sm inline-flex items-center cursor-pointer">
-                  <HeartIcon className="w-5 h-5 mr-1" /> 13
+                <div
+                  className="bg-black/50 rounded-full px-4 py-2 text-sm inline-flex items-center cursor-pointer"
+                  onClick={() => {
+                    setHasLiked(!hasLiked);
+                  }}
+                >
+                  <HeartIcon
+                    className={classNames(
+                      hasLiked ? 'fill-red-500' : null,
+                      'w-5 h-5 mr-1'
+                    )}
+                  />{' '}
+                  {hasLiked ? likes + 1 : likes}
                 </div>
               </div>
             </div>
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -4,13 +4,15 @@ import NtfOverview from '@/components/marketplace/NtfOverview/NtfOverview';
 import Steps from '@/components/marketplace/Steps/Steps';
 
 const Marketplace = () => {
+  const nftMock = { likes: 13 };
+
   return (
     <MainLayout>
       <div className="container px-5 mt-5 lg:mt-20 mx-auto pb-20 lg:pb-30">
         <HeroSection />
         <Steps />
       </div>
-      <NtfOverview />
+      <NtfOverview nftData={nftMock} />
     </MainLayout>
   );
 };
