d557617585c0999f2c40f023e45ae459951670af
M4p4 <homejr@protonmail.com>
2023-02-06 17:19:37 +0100

    wip

---
+++ components/marketplace/PopularCreator/CreatorCard.jsx
@@ -0,0 +1,64 @@
+import Image from 'next/image';
+import React from 'react';
+
+const CreatorCard = ({ creator }) => {
+  const { name, avatar, image, following, eth } = creator;
+  return (
+    <div className="flex flex-col dark:bg-slate-800 bg-white shadow-xl rounded-3xl">
+      <Image
+        className="h-36 object-cover rounded-3xl overflow-hidden"
+        src={image}
+        alt="example image"
+        width={500}
+        height={500}
+      />
+      <div className="p-4">
+        <div className="relative flex items-center justify-center">
+          <div className="absolute">
+            <svg
+              class="mx-auto h-14 -mt-5 text-white dark:text-slate-800 dark:group-hover:text-slate-800"
+              width="134"
+              height="54"
+              viewBox="0 0 134 54"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M101.734 19.8581C99.2658 17.4194 96.9737 14.8065 94.5052 12.1935C94.1526 11.671 93.6237 11.3226 93.0947 10.8C92.7421 10.4516 92.5658 10.2774 92.2131 9.92903C85.6895 3.83226 76.6974 0 67 0C57.3026 0 48.3105 3.83226 41.6105 9.92903C41.2579 10.2774 41.0816 10.4516 40.7289 10.8C40.2 11.3226 39.8474 11.671 39.3184 12.1935C36.85 14.8065 34.5579 17.4194 32.0895 19.8581C23.2737 28.7419 11.4605 30.4839 -0.176331 30.8323V54H16.3974H32.0895H101.558H110.197H134V30.6581C122.363 30.3097 110.55 28.7419 101.734 19.8581Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </div>
+          <div className="absolute mx-auto -top-6">
+            <Image
+              className="h-12 w-12 rounded-full"
+              src={avatar}
+              alt="example image"
+              width={50}
+              height={50}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between items-center mt-7">
+          <div>
+            <div className="block font-semibold">{name}</div>
+            <div className="block text-sm">
+              {eth} <span className="text-gray-400">ETH</span>
+            </div>
+          </div>
+          {following ? (
+            <div className="py-2 px-4 dark:bg-slate-900 bg-white hover:bg-slate-200 dark:hover:bg-slate-800 text-slate-800 border dark:border-gray-500 border-gray-200 rounded-full cursor-pointer text-sm">
+              Following
+            </div>
+          ) : (
+            <div className="py-2 px-4 bg-sky-600 hover:bg-sky-700 rounded-full cursor-pointer text-sm text-slate-100">
+              Follow
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatorCard;
--- components/marketplace/PopularCreator/PopularCreator.jsx
+++ components/marketplace/PopularCreator/PopularCreator.jsx
@@ -5,6 +5,7 @@ import {
   UserPlusIcon,
 } from '@heroicons/react/24/solid';
 import React, { useState } from 'react';
+import CreatorCard from './CreatorCard';
 
 const Buttons = [
   {
@@ -31,6 +32,44 @@ const Buttons = [
   },
 ];
 
+const mockData = [
+  {
+    name: 'Jaro Ratz',
+    eth: '13.337',
+    following: false,
+    avatar: '/images/me.jpg',
+    image: '/images/hero/hero-2.jpeg',
+  },
+  {
+    name: 'Jaro Ratz',
+    eth: '14.337',
+    following: true,
+    avatar: '/images/me.jpg',
+    image: '/images/hero/hero-3.jpeg',
+  },
+  {
+    name: 'Jaro Ratz',
+    eth: '42.337',
+    following: false,
+    avatar: '/images/me.jpg',
+    image: '/images/hero/hero-1.jpeg',
+  },
+  {
+    name: 'Jaro Ratz',
+    eth: '22.337',
+    following: true,
+    avatar: '/images/me.jpg',
+    image: '/images/hero/hero-2.jpeg',
+  },
+  {
+    name: 'Jaro Ratz',
+    eth: '44.337',
+    following: false,
+    avatar: '/images/me.jpg',
+    image: '/images/hero/hero-1.jpeg',
+  },
+];
+
 const PopularCreator = () => {
   const [mode, setMode] = useState(0);
 
@@ -39,23 +78,31 @@ const PopularCreator = () => {
   };
 
   return (
-    <div className="md:container px-5 mt-5 lg:mt-20 mx-auto pb-20 lg:pb-30 bg-black/20 rounded-2xl">
+    <div className="md:container px-5 mt-5 lg:mt-20 mx-auto pb-20 lg:pb-30 dark:bg-black/20 bg-gray-100 rounded-3xl">
       <h2 className="text-4xl text-center font-semibold pt-12 mb-12">
         Daily Top Creators.
       </h2>
 
-      <div className="flex flex-row items-center max-w-2xl mx-auto justify-between bg-slate-700 p-1 rounded-full">
+      <div className="flex flex-row items-center max-w-2xl mx-auto justify-between dark:bg-slate-700 bg-white shadow-xl p-1 rounded-full">
         {Buttons.map((button, i) => (
           <div
             key={i}
             onClick={changeMode.bind(null, i)}
             className={classNames(
-              mode === i ? 'bg-sky-600' : 'hover:text-white',
+              mode === i
+                ? 'bg-sky-600 text-slate-100'
+                : 'dark:hover:text-white hover:text-slate-900 text-slate-700 dark:text-slate-200',
               'inline-flex p-3 rounded-full w-full justify-center items-center cursor-pointer'
             )}
           >
             {button.text}
           </div>
+        ))}
+      </div>
+
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-12">
+        {mockData.map((creator, i) => (
+          <CreatorCard key={i} creator={creator} />
         ))}
       </div>
     </div>
