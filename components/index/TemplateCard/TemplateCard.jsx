a319c5f95c95c159bbc0a9710da5000316a96bcd
M4p4 <homejr@protonmail.com>
2023-02-09 03:35:50 +0100

    index update

---
+++ components/index/TemplateCard/TemplateCard.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const TemplateCard = ({ template }) => {
+  const { image, url, title } = template;
+  return (
+    <Link href={url}>
+      <div className="h-96 w-full border-8 border-orange-700 rounded-3xl hover:border-orange-600 group cursor-pointer">
+        <div className="relative flex items-center h-full w-full justify-center">
+          <Image
+            className="w-full h-full object-cover rounded-2xl"
+            src={image}
+            alt={title}
+            width={400}
+            height={500}
+          />
+          <h2 className="absolute text-xl font-semibold group-hover:text-orange-100 text-slate-200 bg-black/80 p-2 rounded-full max-w-[200px] w-full text-center">
+            {title}
+          </h2>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default TemplateCard;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
-const Home = () => {
-  return <main className="text-xl">Tailwind Templates</main>;
-};
+import TemplateCard from '@/components/index/TemplateCard/TemplateCard';
+import Image from 'next/image';
+import Link from 'next/link';
 
+const templates = [
+  {
+    image: '/images/welcome/nft.jpeg',
+    url: '/marketplace',
+    title: 'NFT Marketplace',
+  },
+  {
+    image: '/images/welcome/blog.jpeg',
+    url: '/blog',
+    title: 'Blog',
+  },
+];
+
+const Home = () => {
+  return (
+    <div className="bg-slate-900 text-slate-200 min-h-screen relative pb-20">
+      <h1 className="font-semibold text-4xl tracking-tight text-center text-slate-100 py-5">
+        Tailwind Templates
+      </h1>
+      <div className="max-w-3xl w-full mx-auto bg-slate-800 text-slate-300 p-5 rounded-3xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          {templates.map((template) => (
+            <TemplateCard key={template.title} template={template} />
+          ))}
+        </div>
+      </div>
+      <div className="absolute bottom-5 w-full text-center">
+        <span className="text-slate-400">Made with ❤️ by Jaro Ratz</span>
+      </div>
+    </div>
+  );
+};
 export default Home;
